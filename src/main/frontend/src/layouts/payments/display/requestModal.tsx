7b649d6ece6f32bc9bf87aa581575ee5d4499ed8
Chung JooYoung <hyundia1947@naver.com>
2024-02-16 17:54:37 +0900

    모달 수정

--- src/main/frontend/src/layouts/payments/display/requestModal.tsx
+++ src/main/frontend/src/layouts/payments/display/requestModal.tsx
@@ -1,6 +1,15 @@
 import React, { useEffect, useState } from "react";
 import "./requestModal.css";
 import axios from "axios";
+import receiptImg from "assets/images/request_img/reciept.png";
+import cardImg from "assets/images/request_img/card.png";
+import clockImg from "assets/images/request_img/clock.png";
+import shopImg from "assets/images/request_img/shop.png";
+import purposeImg from "assets/images/request_img/purpose.png";
+import joinImg from "assets/images/request_img/join.png";
+import picImg from "assets/images/request_img/pic.png";
+import penImg from "assets/images/request_img/pen.png";
+import submitbtn from "assets/images/request_img/submitbtn.png";
 
 interface PaymentInfo {
   paymentTime: string;
@@ -93,6 +102,10 @@ const ModalContext = ({ isOpen, closeModal, selectedPaymentId }: any) => {
     }
   };
 
+  const modalClose = () => {
+    // 모달 닫힘 코드 구현 예정
+  };
+
   const handlePurposeChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
     setSelectedPurpose(parseInt(event.target.value));
   };
@@ -102,48 +115,109 @@ const ModalContext = ({ isOpen, closeModal, selectedPaymentId }: any) => {
   return (
     <div className={isOpen ? "openModal pop" : "pop"}>
       <div className="modal-content">
-        <h1>결재 요청</h1>
-        <div>
-          <div>결제일시</div>
-          <input value={paymentDate} readOnly />
+        <div className="modal-title">
+          <div className="modal-title-img">
+            <img src={receiptImg}></img>
+            <div>
+              <h1>결재 요청</h1>
+              <p style={{ color: "#8e8e8e", fontSize: "0.8rem" }}>
+                {" "}
+                결제 정보가 자동으로 연동됩니다. 필수 정보를 모두 입력해주세요.{" "}
+              </p>
+            </div>
+          </div>
+          <span
+            className="material-icons-round notranslate MuiIcon-root MuiIcon-fontSizeInherit css-fnit94-MuiIcon-root"
+            aria-hidden="true"
+            onClick={modalClose}
+          >
+            close
+          </span>
         </div>
-        <div>
-          <div>사용금액</div>
-          <input value={paymentInfo.payAmount} readOnly />
+        <hr />
+        <div className="modal-main-content">
+          <div className="modal-item">
+            <div className="modal-input">
+              <img src={clockImg} style={{ width: "20px", height: "20px" }} />
+              결제일시
+              <span className="ness"> * </span>
+            </div>
+            <input value={paymentDate} readOnly />
+          </div>
+
+          <div className="modal-item">
+            <div className="modal-input">
+              <img src={cardImg} style={{ width: "20px", height: "20px" }} />
+              사용금액
+              <span className="ness"> * </span>
+            </div>
+            <input value={paymentInfo.payAmount} readOnly />
+          </div>
+
+          <div className="modal-item">
+            <div className="modal-input">
+              <img src={shopImg} style={{ width: "20px", height: "20px" }} />
+              사용처 <span className="ness"> * </span>&nbsp; &nbsp;
+            </div>
+            <input value={paymentInfo.merchant} readOnly />
+          </div>
+
+          <div className="modal-item">
+            <div className="modal-input">
+              <img src={purposeImg} style={{ width: "20px", height: "20px" }} />
+              용도
+              <span className="ness"> * </span> &nbsp; &nbsp;&nbsp; &nbsp;&nbsp;
+            </div>
+            <select onChange={handlePurposeChange}>
+              <option value="">선택하세요</option>
+              {purposeItem.map((purpose: any) => (
+                <option key={purpose.purposeItemUid} value={purpose.purposeItemUid}>
+                  {purpose.purposeCategory} || {purpose.purposeItem}
+                </option>
+              ))}
+            </select>
+          </div>
+
+          <div className="modal-item">
+            <div className="modal-input">
+              <img src={joinImg} style={{ width: "20px", height: "20px" }} />
+              참석자
+              <span className="ness"> * </span>
+            </div>
+            <input
+              placeholder="참석자를 입력하세요."
+              value={participant}
+              onChange={(e) => setParticipant(e.target.value)}
+            />
+          </div>
+
+          <div className="modal-item">
+            <div className="modal-input">
+              <img src={picImg} style={{ width: "20px", height: "20px" }} />
+              영수증 &nbsp; &nbsp;
+            </div>
+            <input type="file" onChange={handleFileChange} />
+          </div>
+
+          <div className="modal-item">
+            <div className="modal-input">
+              <img src={penImg} style={{ width: "20px", height: "20px" }} />
+              메모 &nbsp; &nbsp;&nbsp; &nbsp;&nbsp;
+            </div>
+            <div>
+              <textarea className="inputbox" />
+            </div>
+          </div>
         </div>
+
         <div>
-          <div>사용처</div>
-          <input value={paymentInfo.merchant} readOnly />
+          <button className="submitButton" onClick={handleSubmit}>
+            <img src={submitbtn} />
+          </button>
         </div>
-        <div>
-          <div>용도</div>
-          <select onChange={handlePurposeChange}>
-            <option value="">선택하세요</option>
-            {purposeItem.map((purpose: any) => (
-              <option key={purpose.purposeItemUid} value={purpose.purposeItemUid}>
-                {purpose.purposeCategory} || {purpose.purposeItem}
-              </option>
-            ))}
-          </select>
-        </div>
-        <div>
-          <div>참석자</div>
-          <input
-            placeholder="참석자를 입력하세요."
-            value={participant}
-            onChange={(e) => setParticipant(e.target.value)}
-          />
-        </div>
-        <div>
-          <div>영수증 첨부</div>
-          <input type="file" onChange={handleFileChange} />
-        </div>
-        <button className="submitButton" onClick={handleSubmit}>
-          신청
-        </button>
-        <button className="closeButton" onClick={closeModal}>
+        {/* <button className="closeButton" onClick={closeModal}>
           닫기
-        </button>
+        </button> */}
       </div>
     </div>
   );
