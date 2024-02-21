d35b43a7985f0aeca4b939cb8bb4a12df0d130b4
seunggwang <sgroh1997@gmail.com>
2024-02-21 12:18:43 +0900

    자동 추천 기능 보강

--- src/main/java/com/magic4/magicard/repository/RequestRepo.java
+++ src/main/java/com/magic4/magicard/repository/RequestRepo.java
@@ -12,25 +12,30 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.magic4.magicard.vo.Request;
 import org.springframework.data.jpa.repository.Query;
 
-public interface RequestRepo extends JpaRepository<Request, Integer>{
-    List<Request> findByResponseEmployeeEmailAndApprovalStepsOrderByApprovalStepsAsc(String responseEmployeeEmail, ApprovalSteps approvalSteps);
-    List<Request> findByPaymentInfo(PaymentInfo paymentInfo);
-    List<Request> findByEmployeeAndRequestLevel(Employee employee, int requestLevel);
+public interface RequestRepo extends JpaRepository<Request, Integer> {
+        List<Request> findByResponseEmployeeEmailAndApprovalStepsOrderByApprovalStepsAsc(String responseEmployeeEmail,
+                        ApprovalSteps approvalSteps);
 
-    @Query(nativeQuery = true,
-            value = "SELECT r.*, p.payment_time " +
-                    "FROM request r LEFT JOIN payment_info p " +
-                    "on r.payment_id = p.payment_id " +
-                    "where r.request_employee_email = ? and r.request_level = ? " +
-                    "order by p.payment_time desc")
-    List<Request> findByRequestEmployeeEmailAndRequestLevelOrderBYPaymentTime(String requestEmployeeEmail, int requestLevel);
+        List<Request> findByPaymentInfo(PaymentInfo paymentInfo);
 
-    @Query(nativeQuery = true,
-            value = "SELECT r.*, p.payment_time " +
-                    "FROM request r LEFT JOIN payment_info p " +
-                    "on r.payment_id = p.payment_id " +
-                    "where r.response_employee_email = ? and r.request_level = ? " +
-                    "order by p.payment_time desc")
-    List<Request> findByResponseEmployeeEmailAndRequestLevelOrderByPaymentTime(String responseEmployeeEmail, int requestLevel);
+        List<Request> findByEmployeeAndRequestLevel(Employee employee, int requestLevel);
+
+        List<Request> findByApprovalSteps(ApprovalSteps approvalSteps);
+
+        @Query(nativeQuery = true, value = "SELECT r.*, p.payment_time " +
+                        "FROM request r LEFT JOIN payment_info p " +
+                        "on r.payment_id = p.payment_id " +
+                        "where r.request_employee_email = ? and r.request_level = ? " +
+                        "order by p.payment_time desc")
+        List<Request> findByRequestEmployeeEmailAndRequestLevelOrderBYPaymentTime(String requestEmployeeEmail,
+                        int requestLevel);
+
+        @Query(nativeQuery = true, value = "SELECT r.*, p.payment_time " +
+                        "FROM request r LEFT JOIN payment_info p " +
+                        "on r.payment_id = p.payment_id " +
+                        "where r.response_employee_email = ? and r.request_level = ? " +
+                        "order by p.payment_time desc")
+        List<Request> findByResponseEmployeeEmailAndRequestLevelOrderByPaymentTime(String responseEmployeeEmail,
+                        int requestLevel);
 
 }
--- src/main/java/com/magic4/magicard/service/GptService.java
+++ src/main/java/com/magic4/magicard/service/GptService.java
@@ -24,9 +24,13 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.magic4.magicard.dto.GptResultDto;
 import com.magic4.magicard.dto.PaymentInfoDto;
+import com.magic4.magicard.repository.ApprovalStepsRepo;
 import com.magic4.magicard.repository.PaymentInfoRepo;
 import com.magic4.magicard.repository.PurposeCategoryRepo;
+import com.magic4.magicard.repository.RequestRepo;
+import com.magic4.magicard.vo.ApprovalSteps;
 import com.magic4.magicard.vo.PaymentInfo;
+import com.magic4.magicard.vo.Request;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -37,6 +41,12 @@ public class GptService {
     private String secretKey;
     private final RestTemplate restTemplate;
     private static final String END_POINT = "https://api.openai.com/v1/chat/completions";
+
+    @Autowired
+    private RequestRepo requestRepo;
+
+    @Autowired
+    private ApprovalStepsRepo approvalStepsRepo;
 
     @Autowired
     private PaymentInfoRepo paymentInfoRepo;
@@ -64,6 +74,11 @@ public class GptService {
         Timestamp paymentTime = paymentInfo.getPaymentTime();
 
         List<String> messages = new ArrayList<>();
+        ApprovalSteps finalApproved = approvalStepsRepo.findById(3).orElse(null); // 최종 승인
+        assert (finalApproved != null);
+
+        List<Request> finalApprovedRequests = requestRepo.findByApprovalSteps(finalApproved);
+
         String gptPromptHeader = "이 카드 결제는 어떤 분류에 들어가야 하는가?" + System.lineSeparator();
 
         String payInfo = "결제 일시 " + paymentTime + "\r\n" + //
@@ -72,6 +87,16 @@ public class GptService {
                 "\r\n";
 
         String body = "위의 카드 결제와 연관성이 높은 3개를 골라 연관이 높은 순서대로 그 번호를 나열하여라. 각 항목은 ','로 구분하여라. " + System.lineSeparator();
+        body += "아래의 이력을 참고하여 분류하여라." + System.lineSeparator();
+        body += "(결제일시, 사용금액, 사용처) => (소분류 항목)" + System.lineSeparator();
+
+        for (Request r : finalApprovedRequests) {
+            body += "(" + r.getPaymentInfo().getPaymentTime() + ", " + r.getPaymentInfo().getPayAmount() + ", "
+                    + r.getPaymentInfo().getMerchant() + ") => "
+                    // + r.getPurposeItem().getPurposeCategory().getPurposeCategory() + " || "
+                    + r.getPurposeItem().getPurposeItem() + System.lineSeparator();
+        }
+
         body += """
                     다음 중 가장 연관성이 높은 3개를 골라 연관이 높은 순서대로 나열하여라.
                     예를 들어 다음과 같이 제시하면 된다.
@@ -79,6 +104,8 @@ public class GptService {
                     위와 같이 항목은 ','로 구분하여라.
                     답변은 다른 말을 사용하지 말고 단답으로 답하여라.
 
+                    항목들은 다음의 형식으로 제시되어 있다.
+                    '(결제일시, 사용금액, 사용처) => (대분류 항목 || 소분류 항목)'
                     고를 수 있는 항목은 다음과 같다.
                 """
                 + System.lineSeparator();
@@ -112,6 +139,13 @@ public class GptService {
         }
 
         String finalMsg = gptPromptHeader + payInfo + body + candidates;
+
+        finalMsg += System.lineSeparator();
+        finalMsg += """
+                    다시 한번 강조하지만, 답변은 다른 말을 사용하지 말고 단답으로 답하고 3,1,2와 같이 답하여라. 답변에 큰 따옴표(")를 사용하지 말아라
+                """;
+        ;
+        finalMsg += System.lineSeparator();
         System.out.println(finalMsg);
         messages.add(finalMsg);
 
