d45bdd8249685f632687a83b0dfbf76e45118415
Chung JooYoung <hyundia1947@naver.com>
2024-02-25 14:48:02 +0900

    커밋

--- src/main/frontend/src/component_jw/dashboard/departmentalSpendingChart/index.js
+++ src/main/frontend/src/component_jw/dashboard/departmentalSpendingChart/index.js
@@ -1,64 +1,88 @@
-import React,{useState} from 'react';
-import ReactECharts from 'echarts-for-react';
+import React, { useState } from "react";
+import ReactECharts from "echarts-for-react";
 // `echarts`를 직접 import합니다.
-import * as echarts from 'echarts/core';
-import { Card, Grid, MenuItem, Select } from '@mui/material';
-import { List, ListItem, ListItemText } from '@mui/material';
-import './index.css';
-import SoftTypography from 'components/SoftTypography';
+import * as echarts from "echarts/core";
+import { Card, Grid, MenuItem, Select } from "@mui/material";
+import { List, ListItem, ListItemText } from "@mui/material";
+import "./index.css";
+import SoftTypography from "components/SoftTypography";
 
-
-const departments =  [
-  '인사팀', '구매계약팀', '전략기획팀', '재무팀', '인재개발팀', '직원만족팀', '글로벌사업팀', '글로벌개발팀', '뱅킹금융팀', '뱅킹정보팀', '뱅킹모바일팀', '금융보안1팀', '금융보안2팀', '보안컨설팅1팀', '보안컨설팅2팀', '인프라사업팀', '은행인프라팀', '카드인프라팀', '그룹인프라팀', '인프라ASP팀'
+const departments = [
+  "인사팀",
+  "구매계약팀",
+  "전략기획팀",
+  "재무팀",
+  "인재개발팀",
+  "직원만족팀",
+  "글로벌사업팀",
+  "글로벌개발팀",
+  "뱅킹금융팀",
+  "뱅킹정보팀",
+  "뱅킹모바일팀",
+  "금융보안1팀",
+  "금융보안2팀",
+  "보안컨설팅1팀",
+  "보안컨설팅2팀",
+  "인프라사업팀",
+  "은행인프라팀",
+  "카드인프라팀",
+  "그룹인프라팀",
+  "인프라ASP팀",
 ];
 const spend = [
-  220, 182, 191, 234, 290, 330, 310, 123, 442, 321, 90, 149, 210, 122, 133, 334, 198, 123, 125, 220
+  220, 182, 191, 234, 290, 330, 310, 123, 442, 321, 90, 149, 210, 122, 133, 334, 198, 123, 125, 220,
 ];
 const superDepartments = [
-  '경영부문','전략사업부문','디지털개발부문','인프라&보안부문','경영전략본부','글로벌본부','디지털본부','뱅킹Biz본부','정보보호본부','인프라본부'
+  "경영부문",
+  "전략사업부문",
+  "디지털개발부문",
+  "인프라&보안부문",
+  "경영전략본부",
+  "글로벌본부",
+  "디지털본부",
+  "뱅킹Biz본부",
+  "정보보호본부",
+  "인프라본부",
 ];
-
 
 function getTop5Departments(departments, spend) {
   // 부서와 지출 데이터를 객체의 배열로 결합
   const combinedData = departments.map((department, index) => ({
     department: department,
-    spend: spend[index]
+    spend: spend[index],
   }));
   // 지출액으로 내림차순 정렬
   const sortedData = combinedData.sort((a, b) => b.spend - a.spend);
   // 상위 5개 항목 선택
   const top5 = sortedData.slice(0, 5);
-  
+
   return top5;
 }
 const top5Departments = getTop5Departments(departments, spend);
 
-
 const DepartmentalSpendingChart = () => {
-  const [selectedSuperDepartment, setSelectedSuperDepartment] = React.useState('');
-  
+  const [selectedSuperDepartment, setSelectedSuperDepartment] = React.useState("");
 
-  const handleSuperDepartmentChange=(event) => {
+  const handleSuperDepartmentChange = (event) => {
     setSelectedSuperDepartment(event.target.value);
   };
 
   // 데이터와 옵션 설정
   const option = {
     title: {
-      subtext: '스크롤하여 그래프 확대 가능',
+      subtext: "스크롤하여 그래프 확대 가능",
       subtextStyle: {
-        align: 'right' 
+        align: "right",
       },
-      right: '10%', // 오른쪽 여백을 5%로 설정합니다.
-      top: '5%', // 상단 여백을 5%로 설정합니다.
+      right: "10%", // 오른쪽 여백을 5%로 설정합니다.
+      top: "5%", // 상단 여백을 5%로 설정합니다.
       // height:'40rem'
     },
     xAxis: {
       data: departments,
       axisLabel: {
         inside: false,
-        color: '#333',
+        color: "#333",
         fontSize: 14, // 폰트 크기를 설정
         fontWeight: 600, // 상대적으로 더 두꺼운 폰트
         // interval: 0, // 모든 레이블을 강제로 표시
@@ -70,7 +94,6 @@ const DepartmentalSpendingChart = () => {
         show: false,
       },
       z: 10,
-      
     },
     yAxis: {
       axisLine: {
@@ -80,55 +103,54 @@ const DepartmentalSpendingChart = () => {
         show: false,
       },
       axisLabel: {
-        color: '#999',
+        color: "#999",
       },
     },
     dataZoom: [
       {
-        type: 'inside',
+        type: "inside",
       },
     ],
     series: [
       {
-        type: 'bar',
+        type: "bar",
         showBackground: false,
-        barWidth: '60%', // 막대의 너비 설정
+        barWidth: "60%", // 막대의 너비 설정
         itemStyle: {
           color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
-            { offset: 0, color: '#b7d7c8' },
-            { offset: 0.5, color: '#b7d7c8' },
-            { offset: 1, color: '#cbe1d4' },
+            { offset: 0, color: "#b7d7c8" },
+            { offset: 0.5, color: "#b7d7c8" },
+            { offset: 1, color: "#cbe1d4" },
           ]),
         },
         emphasis: {
           itemStyle: {
             color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
-              { offset: 0, color: '#b8e994' },
-              { offset: 0.5, color: '#58D68D' },
-              { offset: 1, color: '#28B463' },
+              { offset: 0, color: "#b8e994" },
+              { offset: 0.5, color: "#58D68D" },
+              { offset: 1, color: "#28B463" },
             ]),
           },
         },
-        data: spend
-        ,
+        data: spend,
       },
     ],
   };
 
-
-
   return (
     <Card>
-      <Grid container spacing={2}  justifyContent="flex-space-evenly" style={{ padding: '1%' }} >
-      <Grid item xs={12}  sm={12}  >
-          <Select 
+      <Grid container spacing={2} justifyContent="flex-space-evenly" style={{ padding: "1%" }}>
+        <Grid item xs={12} sm={12}>
+          <Select
             value={selectedSuperDepartment}
             onChange={handleSuperDepartmentChange}
             displayEmpty
-            >
-            <MenuItem value="" >전체 부서</MenuItem>
-            {superDepartments.map((item,index) => (
-              <MenuItem key={index} value={item}>{item}</MenuItem>  
+          >
+            <MenuItem value="">전체 부서</MenuItem>
+            {superDepartments.map((item, index) => (
+              <MenuItem key={index} value={item}>
+                {item}
+              </MenuItem>
             ))}
           </Select>
         </Grid>
@@ -140,19 +162,44 @@ const DepartmentalSpendingChart = () => {
           </Select>
         </Grid> */}
       </Grid>
-      <Grid container spacing={2} sx={{ position: "relative", ml: "auto" }}  style={{ paddingLeft: '1%' }} >
-        <Grid className="gridContainer" item xs={2.5} style={{paddingTop:'2.75rem'}} >
-          <SoftTypography variant="h5" fontWeight="bold" 
-            gutterBottom mt={3} mb={3}>
+      <Grid
+        container
+        spacing={2}
+        sx={{ position: "relative", ml: "auto" }}
+        style={{ paddingLeft: "1%" }}
+      >
+        <Grid className="gridContainer" item xs={2.5} style={{ paddingTop: "2.75rem" }}>
+          <SoftTypography variant="h5" fontWeight="bold" gutterBottom mt={3} mb={3}>
             부서 전체 지출 TOP5
           </SoftTypography>
           <List>
             {top5Departments.map((item, index) => (
-            <ListItem key={index} className="listItem">
-              <SoftTypography className="department-rank" variant="body2" color="text" fontWeight="medium">{index+1}</SoftTypography>
-              <SoftTypography className="department-name" variant="body2" color="text" fontWeight="medium">{item.department}</SoftTypography>
-              <SoftTypography className="spend-amount" variant="body2" color="text" fontWeight="medium">{item.spend} 만원</SoftTypography>
-            </ListItem>
+              <ListItem key={index} className="listItem">
+                <SoftTypography
+                  className="department-rank"
+                  variant="body2"
+                  color="text"
+                  fontWeight="medium"
+                >
+                  {index + 1}
+                </SoftTypography>
+                <SoftTypography
+                  className="department-name"
+                  variant="body2"
+                  color="text"
+                  fontWeight="medium"
+                >
+                  {item.department}
+                </SoftTypography>
+                <SoftTypography
+                  className="spend-amount"
+                  variant="body2"
+                  color="text"
+                  fontWeight="medium"
+                >
+                  {item.spend} 만원
+                </SoftTypography>
+              </ListItem>
             ))}
           </List>
         </Grid>
@@ -160,7 +207,8 @@ const DepartmentalSpendingChart = () => {
           <ReactECharts option={option} />
         </Grid>
       </Grid>
-    </Card>   
-)};
+    </Card>
+  );
+};
 
 export default DepartmentalSpendingChart;
--- src/main/frontend/src/component_jw/dashboard/spendingByPurposeChart/index.js
+++ src/main/frontend/src/component_jw/dashboard/spendingByPurposeChart/index.js
@@ -1,153 +1,141 @@
-import React, { useState, useEffect } from 'react';
-import ReactECharts from 'echarts-for-react';
+import React, { useState, useEffect } from "react";
+import ReactECharts from "echarts-for-react";
 // `echarts`를 직접 import합니다.
-import * as echarts from 'echarts/core';
-import { Card, Grid, MenuItem, Select } from '@mui/material';
+import * as echarts from "echarts/core";
+import { Card, Grid, MenuItem, Select } from "@mui/material";
 
 const departments = {
-  "인사팀": [120, 132, 101, 134, 90, 230, 210, 310],
-  "재무팀": [220, 182, 191, 234, 290, 330, 310, 410],
+  인사팀: [120, 132, 101, 134, 90, 230, 210, 310],
+  재무팀: [220, 182, 191, 234, 290, 330, 310, 410],
 };
-const dataAxis = ["용도1","용도2","용도3","용도4","용도5","용도6","용도7","용도8"];
-
+const dataAxis = ["용도1", "용도2", "용도3", "용도4", "용도5", "용도6", "용도7", "용도8"];
 
 const SpendingByPurposeChart = () => {
   const [selectedDepartment, setSelectedDepartment] = useState(Object.keys(departments)[0]);
-  const [barOption, setBarOption] = useState({});
+  const [lineOption, setBarOption] = useState({});
   const [pieOption, setPieOption] = useState({});
 
-  useEffect(() => {
-    const data = departments[selectedDepartment];
-    const pieSeriesData = dataAxis.map((item, index) => ({
-      value: data[index], name: item
-    }));
+ //data axios로 가져와서 용도별 총 합계(이번달 -> pie chart) && data axios : 최근 6개월 추이 
 
-    setBarOption ({
-    title: {
-      subtext: '스크롤하여 그래프 확대 가능',
-      subtextStyle: {
-        align: 'right' 
-      },
-      right: '10%', // 오른쪽 여백을 5%로 설정합니다.
-      top: '5%', // 상단 여백을 5%로 설정합니다.
-
-    },
-    xAxis: {
-      data: dataAxis,
-      axisLabel: {
-        inside: false,
-        color: '#333',
-        fontSize: 14, // 폰트 크기를 설정
-        fontWeight: 600, // 상대적으로 더 두꺼운 폰트
-        interval: 0, // 모든 레이블을 강제로 표시
-      },
-      axisTick: {
-        show: false,
-      },
-      axisLine: {
-        show: false,
-      },
-      z: 10,
-      
-    },
-    yAxis: {
-      axisLine: {
-        show: false,
-      },
-      axisTick: {
-        show: false,
-      },
-      axisLabel: {
-        color: '#999',
-      },
-    },
-    dataZoom: [
-      {
-        type: 'inside',
-      },
-    ],
-    series: [
-      {
-        type: 'bar',
-        showBackground: false,
-        barWidth: '60%', // 막대의 너비 설정
-        itemStyle: {
-          color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
-            { offset: 0, color: '#b7d7c8' },
-            { offset: 0.5, color: '#b7d7c8' },
-            { offset: 1, color: '#cbe1d4' },
-          ]),
+    setBarOption({
+      title: {
+        subtext: "스크롤하여 그래프 확대 가능",
+        subtextStyle: {
+          align: "right",
         },
-        emphasis: {
+        right: "10%", // 오른쪽 여백을 5%로 설정합니다.
+        top: "5%", // 상단 여백을 5%로 설정합니다.
+      },
+      xAxis: {
+        data: dataAxis,
+        axisLabel: {
+          inside: false,
+          color: "#333",
+          fontSize: 14, // 폰트 크기를 설정
+          fontWeight: 600, // 상대적으로 더 두꺼운 폰트
+          interval: 0, // 모든 레이블을 강제로 표시
+        },
+        axisTick: {
+          show: false,
+        },
+        axisLine: {
+          show: false,
+        },
+        z: 10,
+      },
+      yAxis: {
+        axisLine: {
+          show: false,
+        },
+        axisTick: {
+          show: false,
+        },
+        axisLabel: {
+          color: "#999",
+        },
+      },
+      dataZoom: [
+        {
+          type: "inside",
+        },
+      ],
+      series: [
+        {
+          type: "line",
+          showBackground: true,
+          // barWidth: "60%", // 막대의 너비 설정
           itemStyle: {
             color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
-              { offset: 0, color: '#b8e994' },
-              { offset: 0.5, color: '#58D68D' },
-              { offset: 1, color: '#28B463' },
+              { offset: 0, color: "#b7d7c8" },
+              { offset: 0.5, color: "#b7d7c8" },
+              { offset: 1, color: "#cbe1d4" },
             ]),
           },
+          emphasis: {
+            itemStyle: {
+              color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+                { offset: 0, color: "#b8e994" },
+                { offset: 0.5, color: "#58D68D" },
+                { offset: 1, color: "#28B463" },
+              ]),
+            },
+          },
+          data: data,
         },
-        data: data,
-      },
-    ],
-  });
+      ],
+    });
 
-  setPieOption({
-    // color: ['#FF0000', '#00FF00', '#0000FF'], // 사용하고 싶은 색상 코드 배열
-    tooltip: { trigger: 'item' },
-    series: [
-      {
-        name: '지출 분포',
-        type: 'pie',
-        radius: '55%',
-        data: pieSeriesData,
-        emphasis: {
-          itemStyle: {
-            shadowBlur: 10,
-            shadowOffsetX: 0,
-            shadowColor: 'rgba(0, 0, 0, 0.5)'
-          }
-        }
-      }
-    ],
-    // 파이 차트 옵션 설정을 여기에 추가
-  });
+    setPieOption({
+      // color: ['#FF0000', '#00FF00', '#0000FF'], // 사용하고 싶은 색상 코드 배열
+      tooltip: { trigger: "item" },
+      series: [
+        {
+          name: "지출 분포",
+          type: "pie",
+          radius: "55%",
+          data: pieSeriesData,
+          emphasis: {
+            itemStyle: {
+              shadowBlur: 10,
+              shadowOffsetX: 0,
+              shadowColor: "rgba(0, 0, 0, 0.5)",
+            },
+          },
+        },
+      ],
+      // 파이 차트 옵션 설정을 여기에 추가
+    });
+  }, [selectedDepartment]);
 
-}, [selectedDepartment]);
-
-
-
-const handleDepartmentChange = (event) => {
-  setSelectedDepartment(event.target.value);
-};
+  const handleDepartmentChange = (event) => {
+    setSelectedDepartment(event.target.value);
+  };
 
   return (
     <Card>
-      <Grid container spacing={2} >
+      <Grid container spacing={2}>
         <Grid item xs={12}>
-          <Grid container justifyContent="flex-space-evenly" style={{ padding: '16px' }}>
-            <Select
-              value={selectedDepartment}
-              onChange={handleDepartmentChange}
-              displayEmpty
-            >
+          <Grid container justifyContent="flex-space-evenly" style={{ padding: "16px" }}>
+            <Select value={selectedDepartment} onChange={handleDepartmentChange} displayEmpty>
               {Object.keys(departments).map((department) => (
-                <MenuItem key={department} value={department}>{department}</MenuItem>
+                <MenuItem key={department} value={department}>
+                  {department}
+                </MenuItem>
               ))}
             </Select>
           </Grid>
         </Grid>
         <Grid container spacing={2}>
-  <Grid item xs={12} md={4} style={{ display: 'flex', justifyContent: 'center' }}>
-    <ReactECharts option={pieOption} style={{ height: '400px', width: '100%' }} />
-  </Grid>
-  <Grid item xs={12} md={8} style={{ display: 'flex', justifyContent: 'center' }}>
-    <ReactECharts option={barOption} style={{ height: '400px', width: '100%' }} />
-  </Grid>
-</Grid>
+          <Grid item xs={12} md={4} style={{ display: "flex", justifyContent: "center" }}>
+            <ReactECharts option={pieOption} style={{ height: "400px", width: "100%" }} />
+          </Grid>
+          <Grid item xs={12} md={8} style={{ display: "flex", justifyContent: "center" }}>
+            <ReactECharts option={barOption} style={{ height: "400px", width: "100%" }} />
+          </Grid>
+        </Grid>
       </Grid>
     </Card>
-  );      
+  );
 };
 
 export default SpendingByPurposeChart;
