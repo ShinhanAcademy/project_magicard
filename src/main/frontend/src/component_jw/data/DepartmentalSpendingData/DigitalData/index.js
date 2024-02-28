eaf1fbec01368f4d43ed00a8fcc7626ce06040f1
zhengyou628 <150200583+zhengyou628@users.noreply.github.com>
2024-02-28 18:28:30 +0900

    dashboard_barchart_fix CSS

--- src/main/frontend/src/component_jw/data/DepartmentalSpendingData/DigitalData/index.js
+++ src/main/frontend/src/component_jw/data/DepartmentalSpendingData/DigitalData/index.js
@@ -74,7 +74,7 @@ const DigitalData = () => {
             data: departmentCategoryData.map((data) => data.spend.인건비),
             stack: "비용",
             itemStyle: {
-            color: "#b7d7c8",
+            color: "#b0b0ff",
             },
             emphasis: {
             focus: "series",
@@ -92,7 +92,7 @@ const DigitalData = () => {
             data: departmentCategoryData.map((data) => data.spend.행사비),
             stack: "비용",
             itemStyle: {
-            color: "#ffd700",
+            color: "#b0ddff",
             },
             emphasis: {
             focus: "series",
@@ -109,7 +109,7 @@ const DigitalData = () => {
             data: departmentCategoryData.map((data) => data.spend.식비),
             stack: "비용",
             itemStyle: {
-            color: "#36454F",
+            color: "#e4b0d6",
             },
             emphasis: {
             focus: "series",
--- src/main/frontend/src/component_jw/data/DepartmentalSpendingData/StrategicBusinessData/index.js
+++ src/main/frontend/src/component_jw/data/DepartmentalSpendingData/StrategicBusinessData/index.js
@@ -75,7 +75,7 @@ const StrategicBusinessData = () => {
             data: departmentCategoryData.map((data) => data.spend.인건비),
             stack: "비용",
             itemStyle: {
-            color: "#b7d7c8",
+            color: "#b0b0ff",
             },
             emphasis: {
             focus: "series",
@@ -93,7 +93,7 @@ const StrategicBusinessData = () => {
             data: departmentCategoryData.map((data) => data.spend.행사비),
             stack: "비용",
             itemStyle: {
-            color: "#ffd700",
+            color: "#b0ddff",
             },
             emphasis: {
             focus: "series",
@@ -110,7 +110,7 @@ const StrategicBusinessData = () => {
             data: departmentCategoryData.map((data) => data.spend.식비),
             stack: "비용",
             itemStyle: {
-            color: "#36454F",
+            color: "#e4b0d6",
             },
             emphasis: {
             focus: "series",
